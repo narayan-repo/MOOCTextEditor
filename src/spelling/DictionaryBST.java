a5c3ce6454fa88bce58fa827e47c247c22c6830d
Narayan Maity <nero.personalrepo@gmail.com>
2023-04-30 21:08:29 +0530

    Implemented week 5 part 1
    completed part1 of programming assignment for week 5. added code for all the methods in both DictLL and DictBST classes

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -10,8 +10,9 @@ public class DictionaryBST implements Dictionary
 {
    private TreeSet<String> dict;
 	
-    // TODO: Implement the dictionary interface using a TreeSet.  
- 	// You'll need a constructor here
+   public DictionaryBST() {
+	   dict = new TreeSet<>();
+   }
 	
     
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -20,7 +21,10 @@ public class DictionaryBST implements Dictionary
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
+    	if(!dict.contains(word.toLowerCase())) {
+    		dict.add(word.toLowerCase());
+    		return true;
+    	}
         return false;
     }
 
@@ -28,14 +32,12 @@ public class DictionaryBST implements Dictionary
     /** Return the number of words in the dictionary */
     public int size()
     {
-    	// TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
+        return dict.contains(s.toLowerCase());
     }
 
 }
